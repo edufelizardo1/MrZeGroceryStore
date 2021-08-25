28d829ce2768b83f020fda43faba3bb7a11e352f
edufelizardo <edufelizardo1@gamil.com>
2021-08-25 13:00:25 -0300

    Create: design interface

--- MrZeGroceryStore/Login.Designer.cs
+++ MrZeGroceryStore/Login.Designer.cs
@@ -29,22 +29,203 @@ namespace MrZeGroceryStore
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlBarraDeControleSuperior = new System.Windows.Forms.Panel();
+            this.pnlLogotipo = new System.Windows.Forms.Panel();
+            this.lblMercearia = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.lblStatusMenssage = new System.Windows.Forms.Label();
+            this.pnlBarraDeControleSuperior.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlBarraDeControleSuperior
+            // 
+            this.pnlBarraDeControleSuperior.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlBarraDeControleSuperior.BackColor = System.Drawing.Color.Moccasin;
+            this.pnlBarraDeControleSuperior.Controls.Add(this.pnlLogotipo);
+            this.pnlBarraDeControleSuperior.Location = new System.Drawing.Point(0, 0);
+            this.pnlBarraDeControleSuperior.Name = "pnlBarraDeControleSuperior";
+            this.pnlBarraDeControleSuperior.Size = new System.Drawing.Size(391, 25);
+            this.pnlBarraDeControleSuperior.TabIndex = 0;
+            // 
+            // pnlLogotipo
+            // 
+            this.pnlLogotipo.BackColor = System.Drawing.Color.Firebrick;
+            this.pnlLogotipo.Location = new System.Drawing.Point(4, 1);
+            this.pnlLogotipo.Name = "pnlLogotipo";
+            this.pnlLogotipo.Size = new System.Drawing.Size(100, 23);
+            this.pnlLogotipo.TabIndex = 0;
+            // 
+            // lblMercearia
+            // 
+            this.lblMercearia.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMercearia.AutoSize = true;
+            this.lblMercearia.BackColor = System.Drawing.Color.Transparent;
+            this.lblMercearia.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMercearia.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblMercearia.Location = new System.Drawing.Point(150, 25);
+            this.lblMercearia.Name = "lblMercearia";
+            this.lblMercearia.Size = new System.Drawing.Size(96, 56);
+            this.lblMercearia.TabIndex = 1;
+            this.lblMercearia.Text = "Merceária \r\nSeu Zé";
+            this.lblMercearia.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsuario.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblUsuario.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblUsuario.Location = new System.Drawing.Point(52, 79);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(77, 28);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuário:";
+            this.lblUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSenha.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblSenha.Location = new System.Drawing.Point(52, 112);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(65, 28);
+            this.lblSenha.TabIndex = 3;
+            this.lblSenha.Text = "Senha:";
+            this.lblSenha.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Firebrick;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.Moccasin;
+            this.btnLogin.Location = new System.Drawing.Point(140, 209);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(110, 34);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Moccasin;
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUsuario.ForeColor = System.Drawing.Color.Firebrick;
+            this.txtUsuario.Location = new System.Drawing.Point(124, 80);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(202, 26);
+            this.txtUsuario.TabIndex = 5;
+            this.txtUsuario.Text = "Digite seu Usuário";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.Moccasin;
+            this.txtSenha.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSenha.ForeColor = System.Drawing.Color.Firebrick;
+            this.txtSenha.Location = new System.Drawing.Point(124, 113);
+            this.txtSenha.MaxLength = 10;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(202, 26);
+            this.txtSenha.TabIndex = 6;
+            this.txtSenha.Text = "Digite sua Senha";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Firebrick;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Century Gothic", 3F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.ForeColor = System.Drawing.Color.Firebrick;
+            this.btnClose.Location = new System.Drawing.Point(365, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(23, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.BackColor = System.Drawing.Color.Firebrick;
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.Font = new System.Drawing.Font("Century Gothic", 3F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMinimizar.ForeColor = System.Drawing.Color.Firebrick;
+            this.btnMinimizar.Location = new System.Drawing.Point(340, 1);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(23, 23);
+            this.btnMinimizar.TabIndex = 8;
+            this.btnMinimizar.Text = "Minimizar";
+            this.btnMinimizar.UseVisualStyleBackColor = false;
+            // 
+            // lblStatusMenssage
+            // 
+            this.lblStatusMenssage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatusMenssage.AutoSize = true;
+            this.lblStatusMenssage.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatusMenssage.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblStatusMenssage.ForeColor = System.Drawing.Color.Brown;
+            this.lblStatusMenssage.Location = new System.Drawing.Point(52, 152);
+            this.lblStatusMenssage.Name = "lblStatusMenssage";
+            this.lblStatusMenssage.Size = new System.Drawing.Size(133, 28);
+            this.lblStatusMenssage.TabIndex = 9;
+            this.lblStatusMenssage.Text = "Status do login";
+            this.lblStatusMenssage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
             this.ClientSize = new System.Drawing.Size(391, 255);
+            this.Controls.Add(this.lblStatusMenssage);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblMercearia);
+            this.Controls.Add(this.pnlBarraDeControleSuperior);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.pnlBarraDeControleSuperior.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlBarraDeControleSuperior;
+        private System.Windows.Forms.Label lblMercearia;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Panel pnlLogotipo;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.Label lblStatusMenssage;
     }
 }
 
