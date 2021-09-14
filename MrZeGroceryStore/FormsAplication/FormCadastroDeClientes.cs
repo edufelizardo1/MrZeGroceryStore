8958c789110fcd3e80be3553eb97e2983621b3ce
edufelizardo <edufelizardo1@gamil.com>
2021-09-14 08:35:47 -0300

    Create FormCadastroDeClientes.cs

---
+++ MrZeGroceryStore/FormsAplication/FormCadastroDeClientes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MrZeGroceryStore.FormsAplication
+{
+    public partial class FormCadastroDeClientes : Form
+    {
+        public FormCadastroDeClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void mrTextBox11__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mrTextBox6__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
